5f31aaad08ccd432e1a9da9077ec97ec0a9f1441
Guy haiby <ghaiby@ucsc.edu>
2024-04-16 21:14:00 -0700

    finished exp 2

--- experiment2/js/project.js
+++ experiment2/js/project.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Guy Haiby
+// Date: 4/16/24
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,30 +1,16 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - contain js code for drawing from glitch
+// Author: Guy Haiby
+// Date: 4/16/24
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+/* exported setup, draw */
+let seed = 0;
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
+const sandColor = "#A48571" ;
+const skyColor = "#7DC1F3" ;
+const cloudColor = "#F5F5F6" ;
 
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
@@ -34,46 +20,77 @@ function resizeScreen() {
   // redrawCanvas(); // Redraw everything based on new size
 }
 
-// setup() function is called once when the program starts
-function setup() {
-  // place our canvas, making it fit our container
+// listener for reimagine button
+$("#reimagine").click(function() {
+  seed++;
+});
+
+function setup() {  // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
   $(window).resize(function() {
     resizeScreen();
   });
   resizeScreen();
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
+  randomSeed(seed);
+  background(skyColor);
+  
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  
+  drawClouds();
+  
+  //Draw water as rectangle gradient. 
+  for (let i = 0; i < height / 3; i++) {
+    let inter = map(i, 0, height / 3, 0, 1);
+    let c = lerpColor(color(10, 110, 154), color(179, 208, 246), inter);
+    stroke(c);
+    line(0, height / 2 + i,  width, height / 2 + i);
+  }
+  
+  // Draw WaterEdge 
+  fill(sandColor);
+  beginShape();
+  let waterLevel = height / 1.4;
+  let noiseScale = 0.03; 
+  let noiseHeight = height /10;  
+  
+  // Generate wavy line using noise
+  for (let x = 0; x <= width; x += 4) {
+    let y = noise((x + seed * 50) * noiseScale) * noiseHeight; // Perlin noise
+    vertex(x, waterLevel + y);
+  }
+  //start sand at bottom corners of drawing
+  vertex(width, height);
+  vertex(0, height); 
+  endShape(CLOSE);
+  
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function drawClouds() {
+  let numClouds = 5;
+  let time = millis() / 2000; 
+
+  for (let i = 0; i < numClouds; i++) {
+
+    let cloudRadiusX = random(50, 80); 
+    let cloudRadiusY = random(10, 20);
+    let cloudX = (random(0, width) + time * 30) % (width + 100) - 50; // move clouds and wrap 
+    let cloudY = height /3; 
+    let cloudDetail = random(2, 4);  // noise in cloud texture
+    
+    fill(cloudColor);
+    noStroke();
+    beginShape();
+    for (let angle = 0; angle < TWO_PI; angle += radians(1)) {
+      let offset = map(noise(cos(angle) * cloudDetail + seed, sin(angle) * cloudDetail + seed), 0, 1, -12, 12);
+      let x = cloudX + (cloudRadiusX + offset) * cos(angle);
+      let y = cloudY + (cloudRadiusY + offset) * sin(angle);
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+  }
 }
