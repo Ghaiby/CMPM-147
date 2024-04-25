c5d0c2cf58dc56ee959ad8924c7216fd35a6ef59
Guy haiby <ghaiby@ucsc.edu>
2024-04-25 15:58:01 -0700

    2

--- experiment3/js/p2_base.js
+++ experiment3/js/p2_base.js
@@ -3,12 +3,12 @@
 /* global generateGrid drawGrid */
 
 let seed2 = 0;
-let tilesetImage;
-let currentGrid = [];
-let numRows, numCols;
+let tilesetImage2;
+let currentGrid2 = [];
+let numRows2, numCols2;
 
 function preload() {
-  tilesetImage = loadImage(
+  tilesetImage2 = loadImage(
     "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2FtilesetP8.png?v=1611654020438"
   );
 }
@@ -22,12 +22,12 @@ function reseed() {
 }
 
 function regenerateGrid() {
-  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
+  select("#asciiBox").value(gridToString(generateGrid(numCols2, numRows2)));
   reparseGrid();
 }
 
 function reparseGrid() {
-  currentGrid = stringToGrid(select("#asciiBox").value());
+  currentGrid2 = stringToGrid(select("#asciiBox").value());
 }
 
 function gridToString(grid) {
@@ -53,10 +53,10 @@ function stringToGrid(str) {
 }
 
 function setup() {
-  numCols = select("#asciiBox").attribute("rows") | 0;
-  numRows = select("#asciiBox").attribute("cols") | 0;
+  numCols2 = select("#asciiBox").attribute("rows") | 0;
+  numRows2 = select("#asciiBox").attribute("cols") | 0;
 
-  createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
+  createCanvas(16 * numCols2, 16 * numRows2).parent("canvasContainer");
   select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
 
   select("#reseedButton").mousePressed(reseed);
@@ -68,11 +68,11 @@ function setup() {
 
 function draw() {
   randomSeed(seed2);
-  drawGrid(currentGrid);
+  drawGrid(currentGrid2);
 }
 
 function placeTile(i, j, ti, tj) {
-  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
+  image(tilesetImage2, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
 }
 
 
