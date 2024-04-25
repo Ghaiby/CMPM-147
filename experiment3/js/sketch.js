39d24696a21a53fd498ec61258081a89c1291039
Guy haiby <ghaiby@ucsc.edu>
2024-04-24 23:31:26 -0700

    exp 3.2

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,66 +1,39 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+var s = function( p ) { // p could be any variable name
+  var x = 100; 
+  var y = 100;
+  p.setup = function() {
+    p.createCanvas(400, 200);
+  };
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+  p.draw = function() {
+    p.background(0);
+    p.fill(255);
+    p.rect(x,y,50,50);
+  };
+};
+var myp5 = new p5(s, 'c1');
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+// Sketch Two
+var t = function( p ) { 
+  var x = 100.0; 
+  var y = 100; 
+  var speed = 2.5; 
+  p.setup = function() {
+    numCols = select("#asciiBox").attribute("rows") | 0;
+    numRows = select("#asciiBox").attribute("cols") | 0;
+  
+    createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
+    select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
+  
+    select("#reseedButton").mousePressed(reseed);
+    select("#asciiBox").input(reparseGrid);
+  
+    reseed();
+  };
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
-}
-
-// setup() function is called once when the program starts
-function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
-}
-
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+  p.draw = function() {
+    randomSeed(seed);
+    drawGrid(currentGrid);
+  };
+};
+var myp5 = new p5(t, 'c2');
